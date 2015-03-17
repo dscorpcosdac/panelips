4e4277c3f12186c2b13eb50c59e299032c14ffec
elmaspicudo <nypo59@gmail.com>
2015-03-17 16:04:51 -0600

    cascaron

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/restartips", name="restartips")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig', array(
+            'resultado'      => $resultado,
+           
+        ));
+    }
+
+}
