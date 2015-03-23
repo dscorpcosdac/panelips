a9e856b06d26d8c6567be124e3c8087da04fe76f
elmaspicudo <nypo59@gmail.com>
2015-03-23 13:56:09 -0600

    ajax

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,8 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
