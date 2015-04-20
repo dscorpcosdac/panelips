9a214cd180c8f3ab165c59ad3bfa8d6b5071fd86
elmaspicudo <nypo59@gmail.com>
2015-04-20 01:17:56 -0500

    panel

--- src/AppBundle/Controller/SuricataController.php
+++ src/AppBundle/Controller/SuricataController.php
@@ -225,7 +225,7 @@ class SuricataController extends Controller
  //echo base64_decode($rule);
         return $this->render('AppBundle:suricata:edit.html.twig', array(
            'file'=>$file,
-           'rule'=>$rule.trim(utf8_encode((base64_decode($rule))).utf8_decode((base64_decode($rule)))), 
+           'rule'=>base64_decode($rule).trim(utf8_encode((base64_decode($rule))).utf8_decode((base64_decode($rule)))), 
         ));
     }
 
