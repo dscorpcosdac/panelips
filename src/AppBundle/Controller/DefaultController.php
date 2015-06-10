45dcc689b36bcb1e74f3022986afc6abe0c6e4e9
elmaspicudo <nypo59@gmail.com>
2015-06-10 18:51:33 -0500

    reemplazo shorewall por iptables

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -40,15 +40,15 @@ class DefaultController extends Controller
     public function startAction()
     {
         //$resultado=shell_exec('shorewall start');//echo  "hola" | sudo shorewall start 
-        $proceder=shell_exec('sudo /usr/sbin/nsm_sensor_ips-start');
+        $proceder=shell_exec('sudo sh /root/ips-start.sh');
         $pos = strpos($proceder, 'ERROR');
-        if ($pos === false) {
+       /* if ($pos === false) {
             $resultado=shell_exec('sudo shorewall start');
             //$result2=shell_exec('sudo /usr/sbin/nsm_sensor_ips-start');
 
         } else {
             $resultado=$proceder;
-        }
+        }*/
          $body=$this->renderView('default/result.html.twig', array(
            'resultado'      => $proceder.$resultado,               
             ));
@@ -65,16 +65,16 @@ class DefaultController extends Controller
     public function stopAction()
     {
         //$resultado=shell_exec('shorewall start');//echo  "hola" | sudo shorewall start
-        $proceder=shell_exec('sudo shorewall check');
+        $proceder=shell_exec('sudo sh /root/ips-stop.sh');
         $pos = strpos($proceder, 'ERROR');
-        if ($pos === false) {
+       /* if ($pos === false) {
             $result1=shell_exec('sudo shorewall clear');
             $result2=shell_exec('sudo /usr/sbin/nsm_sensor_ps-stop');
              $resultado=$result1.$result2;
 
         } else {
             $resultado=$proceder;
-        }
+        }*/
         $body=$this->renderView('default/result.html.twig', array(
            'resultado'      => $proceder.$resultado,               
             ));
@@ -84,21 +84,45 @@ class DefaultController extends Controller
         return $response;
     }
 
+     /**
+     * @Route("/applyips", name="applyips")
+     */
+    public function applyipsAction()
+    {
+        //$resultado=shell_exec('shorewall start');//echo  "hola" | sudo shorewall start
+        $proceder=shell_exec('sudo sh /root/ips-applych.sh');
+        $pos = strpos($proceder, 'ERROR');
+       /* if ($pos === false) {
+            $result1=shell_exec('sudo shorewall clear');
+            $result2=shell_exec('sudo /usr/sbin/nsm_sensor_ps-stop');
+             $resultado=$result1.$result2;
+
+        } else {
+            $resultado=$proceder;
+        }*/
+        $body=$this->renderView('default/result.html.twig', array(
+           'resultado'      => $proceder.$resultado,               
+            ));
+          $res=json_encode(array('funciono' =>true,'elcont'=> $body));
+        $response = new Response($res);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
     /**
      * @Route("/restarsh", name="restarSh")
      */
     public function restarshAction()
     {
         //$resultado=shell_exec('shorewall start');//echo  "hola" | sudo shorewall start
-        $proceder=shell_exec('sudo shorewall check');
+        $proceder=shell_exec('sudo sh /root/ips-restart.sh');
         $pos = strpos($proceder, 'ERROR');
-        if ($pos === false) {
+       /* if ($pos === false) {
             $resultado=shell_exec('sudo shorewall restart');
            // $resultado=shell_exec('sudo /usr/sbin/nsm_sensor_ips-restart');
 
         } else {
             $resultado=$proceder;
-        }
+        }*/
 
         $body=$this->renderView('default/result.html.twig', array(
            'resultado'      => $proceder.$resultado,               
@@ -130,32 +154,56 @@ class DefaultController extends Controller
         }
         fclose($file);
        */
-        $archivo = '/etc/shorewall/maclist';
+        $contenido='';
+        $archivo = '/etc/maclist/ips_ip';
         $abrir = fopen($archivo,'r+');
         $contenido = fread($abrir,filesize($archivo));
         fclose($abrir);
-         
-        // Separar linea por linea
+        $contenido = explode("\n",$contenido);
+        //print_r($contenido );
+        for($i=0;$i<count($contenido);$i++){   
+            if($contenido[$i]!=''){         
+                $macsips[]=array('ip'=>$contenido[$i],'id'=>$i.'_ip');
+              }
+        }
+
+        $contenido='';
+        $archivo = '/etc/maclist/ips_mac';
+        $abrir = fopen($archivo,'r+');
+        $contenido = fread($abrir,filesize($archivo));
+        fclose($abrir);
+        $contenido = explode("\n",$contenido);
+       // print_r($contenido );
+        for($i=0;$i<count($contenido);$i++){ 
+              if($contenido[$i]!=''){            
+                $macsips[]=array('mac'=>$contenido[$i],'id'=>$i.'_mac');
+              }
+        }
+
+        $contenido='';
+        $archivo = '/etc/maclist/ips_mac_ip';
+        $abrir = fopen($archivo,'r+');
+        $contenido = fread($abrir,filesize($archivo));
+        fclose($abrir);
         $contenido = explode("\n",$contenido);
         for($i=0;$i<count($contenido);$i++){
             $datos=explode('  ', $contenido[$i]);
-            if(count($datos)>2){
-                $datos['id']=$i;
-                $todo[]=$datos;
+            if(count($datos)>1){
+              $macsips[]=array('ip'=>$datos[1],'mac'=>$datos[0],'id'=>$i.'_macip');
             }
-
         }
+        //array_push($todo, $ips, $macs,$macsips);
         $entity  = new File();
         $form = $this->createFormBuilder($entity)
         ->setAction($this->generateUrl('maclist_import'))
-        ->add('nombre', 'choice', array(
+        /*->add('nombre', 'choice', array(
             'choices'   => array('1' => 'Reemplazar todo', '2' => 'Al final del archivo', '3' => 'Al inicio del archivo')
             
-        ))
+        ))*/
         ->add('file')
         ->getForm();
         return $this->render('AppBundle:Etiqueta:index.html.twig', array(
-            'entities' => $todo,
+            'entities' => $macsips,
             'form' => $form->createView(),
         ));
     }
@@ -173,13 +221,27 @@ class DefaultController extends Controller
      */
     public function maclistcreateAction()
     {
-        $archivo = '/etc/shorewall/maclist';
+                
+       $txtMac=trim($this->get('request')->request->get('txtMac', ''));
+       $txtIp=trim($this->get('request')->request->get('txtIp', ''));            
+       //$contenido=trim($txtAction)."  ".trim($txtInterface)."     ".trim($txtMac)."       ".trim($txtIp)."\n";
+       if($txtMac!='' && $txtIp==''){
+          $archivo = '/etc/maclist/ips_mac';
+          $contenido=trim($txtMac)."\n";
+       }
+
+       if($txtMac=='' && $txtIp!=''){
+          $archivo = '/etc/maclist/ips_ip';
+          $contenido=trim($txtIp)."\n";
+       }
+
+       if($txtMac!='' && $txtIp!=''){
+          $archivo = '/etc/maclist/ips_mac_ip';
+          $contenido=trim($txtMac)."  ".trim($txtIp)."\n";
+       }
+
+        //echo $archivo;
         $file = fopen($archivo, "a");
-        $txtAction=$this->get('request')->request->get('txtAction', '');
-            $txtMac=$this->get('request')->request->get('txtMac', '');
-            $txtIp=$this->get('request')->request->get('txtIp', '');
-            $txtInterface=$this->get('request')->request->get('txtInterface', '');
-            $contenido=trim($txtAction)."  ".trim($txtInterface)."     ".trim($txtMac)."       ".trim($txtIp)."\n";
         fwrite($file, $contenido . PHP_EOL);
       //  fwrite($file, "Añadimos línea 2" . PHP_EOL);
         fclose($file);
@@ -191,16 +253,24 @@ class DefaultController extends Controller
      */
     public function maclistdeleteAction()
     {
-         $puntero=$this->get('request')->request->get('cualid', '');
-         $res=false;
+        $puntero=$this->get('request')->request->get('cualid', '');
+        //$puntero=$this->get('request')->request->get('txtElid', '');
+        $operaciones=explode('_', $puntero);
+        $res=false;
+         //if($operaciones[0]){ 
+          switch ($operaciones[1]) {
+            case 'ip':$archivo = '/etc/maclist/ips_ip'; break;
+            case 'mac':$archivo = '/etc/maclist/ips_mac'; break;
+            case 'macip':$archivo = '/etc/maclist/ips_mac_ip'; break;
+          }
         // Separar linea por linea
-        if($puntero>0){ 
-            $archivo = '/etc/shorewall/maclist';
+        //if($puntero>0){ 
+           // $archivo = '/etc/shorewall/maclist';
             $abrir = fopen($archivo,'r+');
             $contenido = fread($abrir,filesize($archivo));
             fclose($abrir);        
             $contenido = explode("\n",$contenido);
-            unset($contenido[$puntero]);
+            unset($contenido[$operaciones[0]]);
             $b = array_values($contenido);
             $otro = implode("\n",$b); 
             // Guardar Archivo
@@ -208,7 +278,7 @@ class DefaultController extends Controller
             fwrite($abrir,$otro);
             fclose($abrir);
             $res=true;
-        }
+        //}
         return $this->redirect($this->generateUrl('maclist'));
         /*$response = new Response(json_encode(array('funciono'=>$res)));
             $response->headers->set('Content-Type', 'application/json');
@@ -222,10 +292,10 @@ class DefaultController extends Controller
     {
          $entity  = new File();
         $form = $this->createFormBuilder($entity)
-        ->add('nombre', 'choice', array(
+        /*->add('nombre', 'choice', array(
             'choices'   => array('1' => 'Reemplazar todo', '2' => 'Al final del archivo', '3' => 'Al inicio del archivo'),
             'required'  => false,
-        ))
+        ))*/
         ->add('file')
         ->getForm();
 //$form['file']->getData()->move('/var/www/mike/seatcrm/seatcrm/src/L2a/FileBundle/Entity/../../../../web/uploads/documents', 'algo.xls');
@@ -240,13 +310,38 @@ class DefaultController extends Controller
             $data = new \Readexcel_Readexcel();
 
             $data->setOutputEncoding('CP1251');
+            $contenidoMac= array();
+            $contenidoIp= array();
+            $contenidoTodo= array();
 
             $data->read($entity->getUploadRootDir().'/'.$entity->getPath());
             for ($i = 1; $i <= $data->sheets[0]['numRows']; $i++) {
-                $contenido[]=trim($data->sheets[0]['cells'][$i][1])."  br0     ".trim($data->sheets[0]['cells'][$i][3])."       ".trim($data->sheets[0]['cells'][$i][4]);
+               // $contenido[]=trim($data->sheets[0]['cells'][$i][1])."  br0     ".trim($data->sheets[0]['cells'][$i][3])."       ".trim($data->sheets[0]['cells'][$i][4]);
                // echo $data->sheets[0]['cells'][$i][1].'<br>';
+              if (array_key_exists(1, $data->sheets[0]['cells'][$i])) {
+                  $txtMac=trim($data->sheets[0]['cells'][$i][1]);
+                }else{
+                  $txtMac='';
+                }
+              if (array_key_exists(2, $data->sheets[0]['cells'][$i])) {
+                $txtIp=trim($data->sheets[0]['cells'][$i][2]);
+              }else{
+                $txtIp='';
+              }
+
+              if($txtMac!='' && $txtIp==''){
+                  $contenidoMac[]=trim($txtMac)."\n";
+               }
+
+               if($txtMac=='' && $txtIp!=''){
+                  $contenidoIp[]=trim($txtIp)."\n";
+               }
+
+               if($txtMac!='' && $txtIp!=''){
+                  $contenidoTodo[]=trim($txtMac)."  ".trim($txtIp)."\n";
+               }
             }
-           switch ($entity->getNombre()) {
+          /* switch ($entity->getNombre()) {
                         case 1:
                             $todo = implode("\n",$contenido); 
                             break;
@@ -271,12 +366,27 @@ class DefaultController extends Controller
                             # code...
                             break;
                     }   
-
+*/
             // Guardar Archivo
-            $archivo = '/etc/shorewall/maclist';
+                   /// print_r($contenidoMac);
+            $txtMac = implode("\n",$contenidoMac);
+            $txtIp = implode("\n",$contenidoIp);
+            $txtTodo = implode("\n",$contenidoTodo);  
+
+            $archivo = '/etc/maclist/ips_mac';
             $abrir = fopen($archivo,'w');
-            fwrite($abrir,$todo);
-            fclose($abrir);      
+            fwrite($abrir,$txtMac);
+            fclose($abrir);   
+
+            $archivo = '/etc/maclist/ips_ip';
+            $abrir = fopen($archivo,'w');
+            fwrite($abrir,$txtIp);
+            fclose($abrir);
+
+            $archivo = '/etc/maclist/ips_mac_ip';
+            $abrir = fopen($archivo,'w');
+            fwrite($abrir,$txtTodo);
+            fclose($abrir);   
     
         }
     }
@@ -292,10 +402,44 @@ class DefaultController extends Controller
      */
     public function maclistexportAction()
     {       
-        $archivo = '/etc/shorewall/maclist';
+       $contenido='';
+        $archivo = '/etc/maclist/ips_ip';
         $abrir = fopen($archivo,'r+');
-        $content = fread($abrir,filesize($archivo));
+        $contenido = fread($abrir,filesize($archivo));
         fclose($abrir);
+        $contenido = explode("\n",$contenido);
+       // print_r($contenido );
+        for($i=0;$i<count($contenido);$i++){   
+            if($contenido[$i]!=''){         
+                $macsips[]=array('ip'=>$contenido[$i],'id'=>$i.'_ip');
+              }
+        }
+
+        $contenido='';
+        $archivo = '/etc/maclist/ips_mac';
+        $abrir = fopen($archivo,'r+');
+        $contenido = fread($abrir,filesize($archivo));
+        fclose($abrir);
+        $contenido = explode("\n",$contenido);
+       // print_r($contenido );
+        for($i=0;$i<count($contenido);$i++){ 
+              if($contenido[$i]!=''){            
+                $macsips[]=array('mac'=>$contenido[$i],'id'=>$i.'_mac');
+              }
+        }
+
+        $contenido='';
+        $archivo = '/etc/maclist/ips_mac_ip';
+        $abrir = fopen($archivo,'r+');
+        $contenido = fread($abrir,filesize($archivo));
+        fclose($abrir);
+        $contenido = explode("\n",$contenido);
+        for($i=0;$i<count($contenido);$i++){
+            $datos=explode('  ', $contenido[$i]);
+            if(count($datos)>1){
+              $macsips[]=array('ip'=>$datos[1],'mac'=>$datos[0],'id'=>$i.'_macip');
+            }
+        }
          
         // Separar linea por linea
         $nombre = "maclist".date('YmdHis');
@@ -319,16 +463,21 @@ header( "Content-disposition: filename=".$nombre.".csv"); */
                         <table>
                           ';
                            // echo $content;
-                              $otros = explode("\n",$content);
+                              //$otros = explode("\n",$content);
                              // print_r($otros);
-                           for($i=0;$i<count($otros);$i++){
-                               $datos=explode('  ', $otros[$i]);
+                           foreach ($macsips as $key ) {                          
                                //print_r($datos);
-                               $contenido.= '<tr><td>'.trim($datos[0]).'</td>';
-                               $contenido.= '<tr><td>'.trim($datos[1]).'</td>';
-                               $contenido.= '<tr><td>'.trim($datos[3]).'</td>';
-                               $contenido.= '<tr><td>'.trim($datos[6]).'</td>';
-                               $i++;
+                              if (array_key_exists('mac', $key)) {
+                                  $contenido.= '<tr><td>'.trim($key['mac']).'</td>';
+                              }else{
+                                  $contenido.= '<tr><td></td>';
+                              }
+                              if (array_key_exists('ip', $key)) {
+                                   $contenido.= '<td>'.trim($key['ip']).'</td></tr>';
+                              }else{
+                                     $contenido.= '<td></td></tr>';
+                              }                              
+                               
                             }
 
                              $contenido.='
@@ -363,26 +512,71 @@ header( "Content-disposition: filename=".$nombre.".csv"); */
     public function maclistupdateAction()
     {
         $puntero=$this->get('request')->request->get('txtElid', '');
-         $res=false;
+        $operaciones=explode('_', $puntero);
+        $res=false;
         // Separar linea por linea
-        if($puntero>0){ 
-            $archivo = '/etc/shorewall/maclist';
-            $abrir = fopen($archivo,'r+');
-            $contenido = fread($abrir,filesize($archivo));
-            fclose($abrir);        
-            $contenido = explode("\n",$contenido);
-            $txtAction=$this->get('request')->request->get('txtAction', '');
-            $txtMac=$this->get('request')->request->get('txtMac', '');
-            $txtIp=$this->get('request')->request->get('txtIp', '');
-            $contenido[$puntero]=trim($txtAction)."  br0     ".trim($txtMac)."       ".trim($txtIp);
-           // $b = array_values($contenido);
-            $otro = implode("\n",$contenido); 
-            // Guardar Archivo
-            $abrir = fopen($archivo,'w');
-            fwrite($abrir,$otro);
-            fclose($abrir);
-            $res=true;
-        }
+        $txtMac=$this->get('request')->request->get('txtMac', '');
+        $txtIp=$this->get('request')->request->get('txtIp', '');
+        //if($operaciones[0]){ 
+          switch ($operaciones[1]) {
+            case 'ip':$archivoOri = '/etc/maclist/ips_ip'; break;
+            case 'mac':$archivoOri = '/etc/maclist/ips_mac'; break;
+            case 'macip':$archivoOri = '/etc/maclist/ips_mac_ip'; break;
+          }
+              if($txtMac!='' && $txtIp==''){
+                  $archivo = '/etc/maclist/ips_mac';
+                  $linea=trim($txtMac)."\n";
+               }
+
+               if($txtMac=='' && $txtIp!=''){
+                  $archivo = '/etc/maclist/ips_ip';
+                  $linea=trim($txtIp)."\n";
+               }
+
+               if($txtMac!='' && $txtIp!=''){
+                  $archivo = '/etc/maclist/ips_mac_ip';
+                  $linea=trim($txtMac)."  ".trim($txtIp)."\n";
+               }
+
+            if($archivo==$archivoOri){
+                  //$archivo = '/etc/shorewall/maclist';
+                  $abrir = fopen($archivo,'r+');
+                  $contenido = fread($abrir,filesize($archivo));
+                  fclose($abrir);        
+                  $contenido = explode("\n",$contenido);
+                  
+                  $contenido[$operaciones[0]]=trim($linea);
+                 // $b = array_values($contenido);
+                  $otro = implode("\n",$contenido); 
+                  // Guardar Archivo
+                  $abrir = fopen($archivo,'w');
+                  fwrite($abrir,$otro);
+                  fclose($abrir);
+                  $res=true;
+            }else{
+                  $abrir = fopen($archivoOri,'r+');
+                  $contenido = fread($abrir,filesize($archivo));
+                  fclose($abrir);        
+                  $contenido = explode("\n",$contenido);
+                  unset($contenido[$operaciones[0]]);
+                  $array = array_values($contenido);
+                  $otro = implode("\n",$array); 
+                  // Guardar Archivo
+                  $abrir = fopen($archivoOri,'w');
+                  fwrite($abrir,$otro);
+                  fclose($abrir);
+
+                   $file = fopen($archivo, "a");
+                   fwrite($file, $linea . PHP_EOL);
+                  //  fwrite($file, "Añadimos línea 2" . PHP_EOL);
+                   fclose($file);
+
+                  $res=true;
+
+               
+            }
+           
+      //  }
        // return $this->redirect($this->generateUrl('maclist'));
         
         $response = new Response(json_encode(array('funciono'=>$res)));
